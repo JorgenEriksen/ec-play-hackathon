09c31d805d02de78c55c4fd0432792d56118dc24
Jorgen Eriksen <jorgen.eriksen93@gmail.com>
2020-06-07 13:19:04 +0200

    liten oppdatering

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseconfig: {
+    apiKey: 'AIzaSyCCm3t0OLKXnNjd8uEWf_s6Xu5GRmp7U0Q',
+    authDomain: 'ec-challenge.firebaseapp.com',
+    databaseURL: 'https://ec-challenge.firebaseio.com',
+    projectId: 'ec-challenge',
+    storageBucket: 'ec-challenge.appspot.com',
+    messagingSenderId: '363271767678',
+    appId: '1:363271767678:web:90de96576850c1bae3fa2f',
+    measurementId: 'G-4W8VE8HJEM'
+  }
 };
